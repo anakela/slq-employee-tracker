52161f5b1177d453a5f72da231b03228ba5a7267
Angie <103882157+anakela@users.noreply.github.com>
2022-07-22 16:12:53 -1000

    Updated server.js to connect to my database.

--- server.js
+++ server.js
@@ -2,6 +2,24 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Use middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: DB_HOST,
+        user: DB_USER,
+        password: DB_PASSWORD,
+        database: 'employees_db',
+    },
+    console.log('Connected to the employees_db database.')
+);
+
 const questArr = [];
 
 function whatToDo() {
@@ -23,17 +41,17 @@ function whatToDo() {
             console.log(answers.selection);
             if (answers.selection = 'View all departments') {
                 viewAllDepts(); // Add function for displaying table showing all departments
-            // } else if (answers.selection = 'View all employees') {
+            } else if (answers.selection = 'View all employees') {
 
-            // } else if (answers.selection = 'Add a department') {
+            } else if (answers.selection = 'Add a department') {
 
-            // } else if (answers.selection = 'Add a role') {
+            } else if (answers.selection = 'Add a role') {
 
-            // } else if (answers.selection = 'Add an employee') {
+            } else if (answers.selection = 'Add an employee') {
 
-            // } else {
+            } else {
 
-            // } 
+            }
         });
 }
 
